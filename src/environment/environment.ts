5459744ea2e91332118c5660a49a83369b4a6305
KevinDuarte12 <100857261+KevinDuarte12@users.noreply.github.com>
2025-03-17 18:58:34 -0500

    hola

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3001/api',
-    endpoint: 'http://localhost:3001/'
+    apiUrl: 'http://localhost:3006/api',
+    endpoint: 'http://localhost:3006/'
   };
